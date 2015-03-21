38336ae13e8f712e5eefad398cf92035bd14872a
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-03-21 17:14:40 +0700

    FIX: Whitespace

--- code/TemplateOverrideExtension.php
+++ code/TemplateOverrideExtension.php
@@ -1,6 +1,4 @@
 <?php
-
-
 /*
 Template override extension for the model
 1) Adds an AlternativeTemplate field
@@ -8,41 +6,38 @@ Template override extension for the model
 */
 class TemplateOverrideExtension extends DataExtension {
 
-  private static $db = array('AlternativeTemplate' => 'Varchar');
+	private static $db = array('AlternativeTemplate' => 'Varchar');
 
 
-  public function updateCMSFields(FieldList $fields) {
-    $templatei18n = _t('TemplateOverride.TEMPLATE', 'Template');
-    $fields->addFieldToTab( 'Root.'.$templatei18n, new TextField( 'AlternativeTemplate',
-        _t('TemplateOverride.ALTERNATIVE_TEMPLATE_NAME', 'Alternative template name') ) );
+	public function updateCMSFields(FieldList $fields) {
+		$templatei18n = _t('TemplateOverride.TEMPLATE', 'Template');
+		$fields->addFieldToTab( 'Root.'.$templatei18n, new TextField( 'AlternativeTemplate',
+				_t('TemplateOverride.ALTERNATIVE_TEMPLATE_NAME', 'Alternative template name') ) );
 
-    $info_field = new LiteralField(
-      $name = 'infofield',
-      $content = '<p>'._t('TemplateOverride.INFO', 'If you wish to change the default template, type the name of the template here.'.
-          'Otherwise the normal default template will be used.  Normally this will not required changing.').'</p>'
-    );
+		$info_field = new LiteralField(
+			$name = 'infofield',
+			$content = '<p>'._t('TemplateOverride.INFO', 'If you wish to change the default'.
+						'template, type the name of the template here.  Otherwise the normal'.
+						'default template will be used.  Normally this will not require '.
+						'changing.</p>'
+		);
 
-    $fields->addFieldToTab( 'Root.'.$templatei18n, $info_field );
-  }
-
-
-
+		$fields->addFieldToTab( 'Root.'.$templatei18n, $info_field );
+	}
 }
-
 
 
 class PageControllerTemplateOverrideExtension extends Extension {
 
-  /*
-  If the alternative template exists, render that, otherwise render with the default template
-  */
-  function index() {
-    $template = $this->owner->AlternativeTemplate;
-    if ( isset($template) && $template != '' ) {
-      return $this->owner->renderWith( array( $this->owner->AlternativeTemplate, 'Page' ) );
-    } else {
-      return array();
-    }
-  }
+	/*
+	If the alternative template exists, render that, otherwise render with the default template
+	*/
+	function index() {
+		$template = $this->owner->AlternativeTemplate;
+		if ( isset($template) && $template != '' ) {
+			return $this->owner->renderWith( array( $this->owner->AlternativeTemplate, 'Page' ) );
+		} else {
+			return array();
+		}
+	}
 }
-?>
