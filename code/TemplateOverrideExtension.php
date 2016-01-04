06224bf1b0638b8250552b3432a994b4a91db1c3
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-04 11:52:58 +0700

    FIX: Only have one class per file as per PSR-2 coding standard

--- code/TemplateOverrideExtension.php
+++ code/TemplateOverrideExtension.php
@@ -25,38 +25,3 @@ class TemplateOverrideExtension extends DataExtension {
 		$fields->addFieldToTab( 'Root.'.$templatei18n, $info_field );
 	}
 }
-
-
-class PageControllerTemplateOverrideExtension extends Extension {
-
-	/*
-	If the alternative template exists, render that, otherwise render with the default template
-	*/
-	function index() {
-		return $this->useTemplateOverride();
-	}
-
-	/**
-	 * Render this page using the template override iff it exists
-	 * @return an array suitable for SilverStripe to use the correct template
-	 */
-	function useTemplateOverride($data = null) {
-		$template = $this->owner->AlternativeTemplate;
-		if ( isset($template) && $template != '' ) {
-			if ($data) {
-				return $this->owner->customise(new ArrayData($data))
-						->renderWith( array( $this->owner->AlternativeTemplate, $this->owner->ClassName, 'Page' ) );
-			} else {
-				return $this->owner->renderWith( array( $this->owner->AlternativeTemplate, $this->owner->ClassName, 'Page' ) );
-			}
-
-		} else {
-			if ($data) {
-				return $data;
-			} else {
-				return array();
-			}
-
-		}
-	}
-}
