9cda8d8734a9b635aa2c21669c2b320c2532fe71
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-04 13:04:08 +0700

    ENHANCEMENT: Tests for the template overriding mechanism

---
+++ tests/PageControllerTemplateOverrideExtensionTest.php
@@ -0,0 +1,53 @@
+<?php
+
+class PageControllerTemplateOverrideExtensionTest extends FunctionalTest {
+
+	protected static $fixture_file = 'template-override/tests/pages.yml';
+
+	public function testLayoutTemplateOveride() {
+		$this->logInWithPermission('ADMIN');
+		$page = $this->objFromFixture('Page', 'page1');
+		$page->AlternativeTemplate = 'PageInnerTest';
+		$page->write();
+		$page->doPublish();
+		$response = $this->get('/' . $page->URLSegment);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        // assert the the inner layout template has been used
+        $this->assertExactMatchBySelector("div.marker", array(
+		    "INNER LAYOUT"
+		));
+	}
+
+
+	public function testOuterTemplateOveride() {
+		$this->logInWithPermission('ADMIN');
+		$page = $this->objFromFixture('Page', 'page1');
+		$page->AlternativeTemplate = 'PageOuterTest';
+		$page->write();
+		$page->doPublish();
+		$response = $this->get('/' . $page->URLSegment);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        // show the the outer layout template has been used
+        $this->assertExactMatchBySelector("div.marker", array(
+		    "OUTER OF LAYOUT"
+		));
+	}
+
+
+	public function testNoTemplateOverride() {
+		$this->logInWithPermission('ADMIN');
+		$page = $this->objFromFixture('Page', 'page1');
+		$page->AlternativeTemplate = null;
+		$page->write();
+		$page->doPublish();
+		$response = $this->get('/' . $page->URLSegment);
+        $this->assertEquals(200, $response->getStatusCode());
+        $body = $response->getBody();
+        $this->assertNotContains('OUTER OF LAYOUT', $body);
+        $this->assertNotContains('INNER LAYOUT', $body);
+	}
+
+
+}
