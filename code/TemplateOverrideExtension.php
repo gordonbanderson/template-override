4a182b325b8920590f6381a9ac794d93b940e525
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-04 16:19:05 +0700

    FIX: Moved PageControllerTemplateOverrideExtension to a separate file

--- code/TemplateOverrideExtension.php
+++ code/TemplateOverrideExtension.php
@@ -11,8 +11,8 @@ class TemplateOverrideExtension extends DataExtension {
 
 	public function updateCMSFields(FieldList $fields) {
 		$templatei18n = _t('TemplateOverride.TEMPLATE', 'Template');
-		$fields->addFieldToTab( 'Root.'.$templatei18n, new TextField( 'AlternativeTemplate',
-				_t('TemplateOverride.ALTERNATIVE_TEMPLATE_NAME', 'Alternative template name') ) );
+		$fields->addFieldToTab('Root.'.$templatei18n, new TextField('AlternativeTemplate',
+				_t('TemplateOverride.ALTERNATIVE_TEMPLATE_NAME', 'Alternative template name')));
 
 		$info_field = new LiteralField(
 			$name = 'infofield',
@@ -22,41 +22,6 @@ class TemplateOverrideExtension extends DataExtension {
 						' require changing.').'</p>'
 		);
 
-		$fields->addFieldToTab( 'Root.'.$templatei18n, $info_field );
-	}
-}
-
-
-class PageControllerTemplateOverrideExtension extends Extension {
-
-	/*
-	If the alternative template exists, render that, otherwise render with the default template
-	*/
-	function index() {
-		return $this->useTemplateOverride();
-	}
-
-	/**
-	 * Render this page using the template override iff it exists
-	 * @return an array suitable for SilverStripe to use the correct template
-	 */
-	function useTemplateOverride($data = null) {
-		$template = $this->owner->AlternativeTemplate;
-		if ( isset($template) && $template != '' ) {
-			if ($data) {
-				return $this->owner->customise(new ArrayData($data))
-						->renderWith( array( $this->owner->AlternativeTemplate, $this->owner->ClassName, 'Page' ) );
-			} else {
-				return $this->owner->renderWith( array( $this->owner->AlternativeTemplate, $this->owner->ClassName, 'Page' ) );
-			}
-
-		} else {
-			if ($data) {
-				return $data;
-			} else {
-				return array();
-			}
-
-		}
+		$fields->addFieldToTab('Root.'.$templatei18n, $info_field);
 	}
 }
